739c0d8cfa71a9dfd15947c93d4c17050a239357
chenaoh <cdhena@gmail.com>
2023-01-31 21:12:29 -0300

    se agrega el proyecto

---
+++ funciones.js
@@ -0,0 +1,30 @@
+
+
+function mostrar1(){
+    document.getElementById('ppp').style.display = 'block';
+}
+function ocultar1(){
+    document.getElementById('ppp').style.display = 'none';
+}
+
+
+function mostrar2(){
+    document.getElementById('jjj').style.display = 'block';
+}
+function ocultar2(){
+    document.getElementById('jjj').style.display = 'none';
+}
+
+function mostrar3(){
+    document.getElementById('hhh').style.display = 'block';
+}
+function ocultar3(){
+    document.getElementById('hhh').style.display = 'none';
+}
+
+function mostrar4(){
+    document.getElementById('lll').style.display = 'block';
+}
+function ocultar4(){
+    document.getElementById('lll').style.display = 'none';
+}
